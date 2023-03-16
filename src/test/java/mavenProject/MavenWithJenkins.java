a71bb8918ae65404c0987e47cc99f4baeca6f384
Veeraveni <18h41a0506cse@gmail.com>
2023-03-16 16:01:07 +0530

    pushing maven file to github

---
+++ src/test/java/mavenProject/MavenWithJenkins.java
@@ -0,0 +1,11 @@
+package mavenProject;
+
+public class MavenWithJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("This is maven project with jenkins and github.");
+		// TODO Auto-generated method stub
+
+	}
+
+}
